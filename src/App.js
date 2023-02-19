266ed127543387f5af5de86b0e721f17d49d0df3
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2023-02-19 12:01:41 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Happy Sunday!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
