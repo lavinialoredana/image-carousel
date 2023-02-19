b91d4b63d4a3db168a4fb48b637cd10e7ed02058
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2023-02-19 21:25:54 +0100

    Create Accordion Page

---
+++ src/pages/Accordion/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Accordion() {
+  return (
+    <div className="container">
+      <h1> FAQ/Accordion</h1>
+      <div>
+        <details>
+          <summary>Is this a good product?</summary>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere
+            in labore maxime, assumenda iure sed tenetur alias omnis eveniet
+            similique laborum, neque porro unde ducimus officiis animi vitae!
+            Quidem.
+          </p>
+        </details>
+      </div>
+    </div>
+  );
+}
+
+export default Accordion;
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+import Accordion from "../pages/Accordion";
 import App from "../pages/App/App";
 
 export const routesConfig = [
   {
     path: "/",
     element: <App />,
+  },
+
+  {
+    path: "/accordion",
+    element: <Accordion />,
   },
 ];
 
