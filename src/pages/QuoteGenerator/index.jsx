6c166efdd110e3aabcfcd1115efe3a2fe223a40c
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2023-02-20 13:02:52 +0100

    Create QuoteGenerator page + Quote component

---
+++ src/pages/QuoteGenerator/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function QuoteGenerator() {
+    return (
+        <div className="container">
+            <h1> Project 3: Quote Generator </h1>
+            <QuoteGenerator />
+        </div>
+    )
+}
+
+export default QuoteGenerator;
