cfe8a14ea57ca7b5a742ecb598c3c728747504d6
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2023-02-21 12:05:34 +0100

    Fetch quotes array

--- src/pages/QuoteGenerator/index.jsx
+++ src/pages/QuoteGenerator/index.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Quote from "../../components/Quote"
 
 function QuoteGenerator() {
+
+    const [quotes, setQuotes] = useState([]);
+
+
+    const fetchQuotes = async () => {
+        const response = await fetch("https://type.fit/api/quotes");
+
+        if(!response.ok) {
+            throw new Error("Data failed to be fetched!")
+        } else {
+            const json = await response.json();
+            console.log("quotesJS", json)
+            return json;
+        }
+    }
+
+    useEffect( () => {
+        fetchQuotes()
+        .then((quotesArray) => {
+            setQuotes(quotesArray)
+        })
+        .catch((e) =>{
+            console.log(e.message)
+        })
+    }, [])
+
     return (
         <div className="container">
             <h1> Project 3: Quote Generator </h1>
