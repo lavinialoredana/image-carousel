d54ad9c04091d9b1e4f354e2b9b56b8cea5b3106
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2023-02-21 12:06:42 +0100

    Spaces cleanup

--- src/components/Details/index.jsx
+++ src/components/Details/index.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 
-
-const Details = ({title, bodyMessage}) => {
-    return(
-
-        <div>
-       <details>
-         <summary>{title}</summary>
-         <p>
-           {bodyMessage}
-         </p>
-       </details>
-     </div>
-)
-}
+const Details = ({ title, bodyMessage }) => {
+  return (
+    <div>
+      <details>
+        <summary>{title}</summary>
+        <p>{bodyMessage}</p>
+      </details>
+    </div>
+  );
+};
 
 export default Details;
