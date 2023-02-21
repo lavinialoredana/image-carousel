0c6bce22530356dee2c9bdf8b445cc2779a39964
LAVINIA SIRBULESCU <lavinia.loredana@thoughtworks.com>
2023-02-21 16:36:31 +0100

    Implement getRandomQuote functionality and pass randomQuote props to Quote component

--- src/components/Quote/index.jsx
+++ src/components/Quote/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Quote = () =>{
-    return ''
-}
+const Quote = ({ randomQuote, onHandleNewQuote }) => {
+  return (
+    <div>
+      <h4>{randomQuote?.text}</h4>
+      <p> {randomQuote?.author}</p>
+      <button onClick={onHandleNewQuote}> Get new quote</button>
+    </div>
+  );
+};
 
 export default Quote;
--- src/pages/QuoteGenerator/index.jsx
+++ src/pages/QuoteGenerator/index.jsx
@@ -1,39 +1,51 @@
 import React, { useEffect, useState } from "react";
-import Quote from "../../components/Quote"
+import Quote from "../../components/Quote";
 
 function QuoteGenerator() {
+  const [quotes, setQuotes] = useState([]);
+  const [randomQuote, setRandomQuote] = useState({ author: "", text: "" });
 
-    const [quotes, setQuotes] = useState([]);
+  const fetchQuotes = async () => {
+    const response = await fetch("https://type.fit/api/quotes");
 
-
-    const fetchQuotes = async () => {
-        const response = await fetch("https://type.fit/api/quotes");
-
-        if(!response.ok) {
-            throw new Error("Data failed to be fetched!")
-        } else {
-            const json = await response.json();
-            console.log("quotesJS", json)
-            return json;
-        }
+    if (!response.ok) {
+      throw new Error("Data failed to be fetched!");
+    } else {
+      const json = await response.json();
+      console.log("quotesJS", json);
+      return json;
     }
+  };
 
-    useEffect( () => {
-        fetchQuotes()
-        .then((quotesArray) => {
-            setQuotes(quotesArray)
-        })
-        .catch((e) =>{
-            console.log(e.message)
-        })
-    }, [])
+  useEffect(() => {
+    fetchQuotes()
+      .then((quotesArray) => {
+        setQuotes(quotesArray);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }, []);
 
-    return (
-        <div className="container">
-            <h1> Project 3: Quote Generator </h1>
-            <Quote />
-        </div>
-    )
+  const getRandomQuote = (array) => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    const randomItem = array[randomIndex];
+
+    return randomItem;
+  };
+
+  const handleNewQuote = () => {
+    setRandomQuote(getRandomQuote(quotes));
+  };
+
+  return (
+    <div className="container">
+      <h1> Project 3: Quote Generator </h1>
+      <Quote 
+        randomQuote={randomQuote} 
+        onHandleNewQuote={handleNewQuote} />
+    </div>
+  );
 }
 
 export default QuoteGenerator;
